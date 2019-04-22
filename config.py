efa5066d68cf90d38e6d5fdc395354e7b0f070d0
dzmitrybutar <verysweetleaf@gmail.com>
2019-04-22 20:21:11 +0300

    Fixed

--- config.py
+++ config.py
@@ -22,6 +22,6 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = ['example@asd.com']
 
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
